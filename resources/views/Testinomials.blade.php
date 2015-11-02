07ba8fdcb783fc6d2500d5d8efd8e133ee7f7354
dmallela <dmallela@localhost.localdomain>
2015-11-01 21:06:54 -0500

    changed

--- resources/views/Testinomials.blade.php
+++ resources/views/Testinomials.blade.php
@@ -6,25 +6,21 @@
 
 <h4>Testinomials:</h4>
 
-<?php
-require_once "Database.php";
-  $d = new Database();
-$testinomials=$d->getTestinomial();
-if(count($testinomials)==0){
-echo "<p> Currently there are no Testinomials to display.</p>";
-}
-echo "<table border='0'>";
-foreach($testinomials as $testinomial){
-echo "<div>";
-echo "<p><b>". $testinomial["name"] ."</b></p>";
-//echo "<>";
-//echo "";
-echo "<p>". $testinomial["testinomial"] ."</p>";
-echo "</div>";
-}
-echo "</table>";
-// header("Location:Testinomials.html");
-?>
-</div>
-</div>
+
+@if($testinomials)
+
+	<table border='0'>
+	 @foreach($testinomials as $testinomial)
+	<div>
+	<p><b>{{ $testinomial->name }} </b></p>
+	<p>{{  $testinomial->testinomial }} </p>
+	</div>
+	@endforeach
+	</table>
+
+	@else
+        <p>no testinomials written at this point</p>
+	
+@endif
+
 @stop
