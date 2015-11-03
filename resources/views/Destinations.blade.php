8cfff8017630f81bc4b511c11f70464d8d04cc02
dmallela <dmallela@localhost.localdomain>
2015-11-02 21:09:10 -0500

    Added destinations

--- resources/views/Destinations.blade.php
+++ resources/views/Destinations.blade.php
@@ -1,5 +1,51 @@
 @extends('layout')
 
 @section('content')
-	Users!
+	<div class="content">
+<h2>Destinations Where we can reach: </h2>
+Currently destinations where we can reach are listed below:<br/><br/>
+	<table>
+	<tr>
+<td>Alabama</td>
+<td>Alaska</td>
+<td>Arizona</td>
+<td>Arkansas</td>
+</tr>
+<tr>
+<td>California</td>
+<td>Colorado</td>
+<td>Connecticut</td>
+<td>Delaware</td>
+</tr>
+<tr>
+<td>Florida</td>
+<td>Georgia</td>
+<td>Hawaii</td>
+<td>Idaho</td>
+</tr>
+<tr>
+<td>Illinois</td>
+<td>Indiana</td>
+<td>Iowa</td>	<td>Kansas</td>
+</tr>
+<tr><td>Kentucky</td>		<td>Louisiana</td>	<td>Maine</td>		<td>Maryland</td></tr>
+<tr><td>Massachusetts</td>		<td>Michigan</td>	<td>Minnesota</td>	<td>Mississippi</td></tr>
+<tr><td>Missouri</td>		<td>Montana</td>	<td>Nebraska</td>	<td>Nevada</td></tr>
+<tr><td>New Hampshire</td>		<td>New Jersey</td>	<td>New Mexico</td>	<td>New York</td></tr>
+<tr><td>North Carolina</td>		<td>North Dakota</td>	<td>Ohio</td>		<td>Oklahoma</td></tr>
+<tr><td>Oregon</td>			<td>Pennsylvania</td>	<td>Rhode Island</td>	<td>South Carolina</td></tr>
+<tr><td>South Dakota</td>		<td>Tennessee</td>	<td>Texas</td>		<td>Utah</td></tr>
+<tr><td>Vermont</td>		<td>Virginia</td>		<td>Washington</td>	<td>West Virginia</td></tr>
+<tr><td>Wisconsin</td>		<td>Wyoming</td>
+
+
+<td></td>
+<td></td>
+</tr>
+
+</table>	
+
+
+</div>
+</div>
 @stop
